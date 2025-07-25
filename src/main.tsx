3f0c5fe9ddd411ed5db563fc165f2c737d996404
Chaun Ouamthip <32265715+champseo2017@users.noreply.github.com>
2025-07-26 00:31:56 +0700

    update add ant-design

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
+import { StyleProvider } from '@ant-design/cssinjs'
+import { App as AntApp, ConfigProvider } from 'antd'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import 'antd/dist/reset.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <ConfigProvider wave={{ disabled: true }}> 
+      <StyleProvider hashPriority="high">
+        <AntApp>
+          <App />
+        </AntApp>
+      </StyleProvider>
+    </ConfigProvider>
 )
