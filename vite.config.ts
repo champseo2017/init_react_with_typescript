6e516013673a2a45843fc41fb0d793f9fe9a8089
Chaun Ouamthip <32265715+champseo2017@users.noreply.github.com>
2025-07-26 00:22:45 +0700

    init Alias Path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { defineConfig } from 'vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@Components': path.resolve(__dirname, './src/components'),
+      '@Services': path.resolve(__dirname, './src/services'),
+      '@Utils': path.resolve(__dirname, './src/utils'),
+      '@Hooks': path.resolve(__dirname, './src/hooks'),
+      '@Types': path.resolve(__dirname, './src/types'),
+      '@Assets': path.resolve(__dirname, './src/assets'),
+      '@Styles': path.resolve(__dirname, './src/styles'),
+      '@Pages': path.resolve(__dirname, './src/pages'),
+    }
+  },
+  build: {
+    sourcemap: false,
+  }
 })
